6cc3bce49c05eca7f51f17b6f655e77225d43e00
masud520 <masud.abdi@student.moringaschool.com>
2023-10-23 10:01:11 +0300

    completed the assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+function domLoaded(){
+    document.addEventListener('DOMContentLoaded', function(){
+        let p = document.querySelector('#text');
+        p.textContent = 'This is really cool!';
+
+    })
+}
+domLoaded()
